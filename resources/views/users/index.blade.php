d917a761188e707574cbc13978d81c8d7684da21
Hiroaki Takeshima <htakeshimajp@gmail.com>
2019-03-18 23:34:48 +0900

    finish lesson 39

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,30 +3,58 @@
 @section('title', "users")
 
 @section('content')
-	
-	<h1>{{ $title }}</h1>
-	<hr>
 
-	<p>
-		<a href="{{ route('users.create') }}">New user</a>
-	</p>
-		
-	<ul>
-		@forelse ($users as $user)
-			<li>{{ $user->name }},{{ $user->email }}
-				<a href="{{ route('users.show', $user) }}">More details</a> |
-				<a href="{{ route('users.edit', $user) }}">Edit</a> |
+	<div class="d-flex justify-content-between align-items-end mb-2">
+		<h1 class="pb-1">{{ $title }}</h1>
+
+		<p>
+			<a href="{{ route('users.create') }}" class="btn btn-primary">New user</a> 
+		</p>
+
+	</div>
+	
+	
+
+	@if($users->isNotEmpty())
+
+	<table class="table">
+	  <thead class="thead-dark">
+	    <tr>
+	      <th scope="col">#</th>
+	      <th scope="col">Name</th>
+	      <th scope="col">Email</th>
+	      <th scope="col">Action</th>
+	    </tr>
+	  </thead>
+	  <tbody>
+
+	  @foreach($users as $user)	
+	    <tr>
+	      <th scope="row">{{ $user->id }}</th>
+	      <td>{{ $user->name}}</td>
+	      <td>{{ $user->email }}</td>
+	      <td> 
+	      		
 				<form action="{{ route('users.destroy',$user)}}" method="POST">
 					{{ csrf_field() }}
 					{{ method_field('DELETE') }}
-					<button type="submit">Delete</button>
-				</form>
-			</li>
-		@empty
-			<p>No registered users.</p>	
-		@endforelse
-	</ul>
+					<a href="{{ route('users.show', $user) }}" class="btn btn-link"><span class="oi oi-eye"></span></a> 
+					<a href="{{ route('users.edit', $user) }}" class="btn btn-link"><span class="oi oi-pencil"></span></a> 
+					<button type="submit" class="btn btn-link"><span class="oi oi-trash"></span></button>
+				</form>		
+	      </td>
+	    </tr>
+	    
+	  @endforeach	
 
+	  </tbody>
+	</table>
+	
+	@else
+		<p>There are no registered users</p>
+	@endif
+
+	
 @endsection
 
 
